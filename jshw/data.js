4f011b0e468b42dfc15844689b78fe2b6bef4c32
Алексей Якунин <116656993+Womxid@users.noreply.github.com>
2023-02-02 16:28:47 +0300

    Add files via upload

---
+++ jshw/data.js
@@ -0,0 +1,59 @@
+const dataInfo = `[
+
+        
+    {
+        "url": "https://github.com/Womxid/HW.HTML-CSS.7/blob/main/HW7/img/Image.png?raw=true",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "width": 300,
+        "product" : "product1",
+        "height": 450
+        
+    },
+    {
+        "url": "https://github.com/Womxid/HW.HTML-CSS.7/blob/main/HW7/img/Rectangle%2015%20copy%20(1).png?raw=true",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "width": 300,
+        "product" : "product2",
+        "height": 450
+    },
+    {
+        "url": "https://github.com/Womxid/HW.HTML-CSS.7/blob/main/HW7/img/Rectangle%2015%20copy%20(2).png?raw=true",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "product" : "product3",
+        "width": 300,
+        "height": 450
+    },
+    {
+        "url": "https://github.com/Womxid/HW.HTML-CSS.7/blob/main/HW7/img/Rectangle%2015%20copy%20(3).png?raw=true",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "product" : "product4",
+        "width": 300,
+        "height": 450
+    },
+    {
+        "url": "https://github.com/Womxid/HW.HTML-CSS.7/blob/main/HW7/img/Rectangle%2015%20copy%20(4).png?raw=true",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "product" : "product5",
+        "width": 300,
+        "height": 450
+    },
+    {
+        "url": "https://github.com/Womxid/HW.HTML-CSS.7/blob/main/HW7/img/Rectangle%2015%20copy.png?raw=true",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "product" : "product6",
+        "width": 300,
+        "height": 450
+    }
+]`;
